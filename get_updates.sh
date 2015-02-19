ace41b6adf97b625064c4d2d0b1cfe27bd529fad
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2015-02-19 14:16:31 +0100

    Added install script

--- get_updates.sh
+++
@@ -1,9 +0,0 @@
-cp ~/.bashrc .
-cp ~/.fehbg .
-cp -rf ~/.i3 .
-cp -rf ~/.vim .
-cp ~/.vimrc .
-cp ~/.Xdefaults .
-cp ~/.xinitrc .
-cp -rf ~/.zsh .
-cp ~/.zshrc .
---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+cd $HOME
+ln -s $OLDPWD/.vimrc .
+ln -s $OLDPWD/.vim .
+ln -s $OLDPWD/.zshrc .
+ln -s $OLDPWD/.zsh .
+ln -s $OLDPWD/.Xdefaults .
+ln -s $OLDPWD/.gitconfig .
+ln -s $OLDPWD/.bashrc .
