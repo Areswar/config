6ee03657dc550c667fc649af1936c5787b8b09a1
Pierre-Alexandre Veyry <paveyry@arista.com>
2023-05-29 16:52:30 +0100

    repo: install: update install script with new conf and more checks

--- install.sh
+++ install.sh
@@ -5,11 +5,13 @@
 
 # To run it, chdir inside the repository, and execute ./install.sh
 
+set -e
 
 cd $HOME
-ln -s $OLDPWD/dotfiles/vimrc .vimrc 
-ln -s $OLDPWD/dotfiles/vim .vim 
-ln -s $OLDPWD/dotfiles/zshrc .zshrc 
-ln -s $OLDPWD/dotfiles/zsh .zsh 
-ln -s $OLDPWD/dotfiles/gitconfig .gitconfig 
-ln -s $OLDPWD/dotfiles/bashrc .bashrc 
+[ ! -d ".vimrc" ] && ln -s $OLDPWD/dotfiles/vimrc .vimrc || echo ".vimrc already exists"
+[ ! -d ".vim" ] && ln -s $OLDPWD/dotfiles/vim .vim || echo ".vim already exists"
+[ ! -d ".zshrc" ] && ln -s $OLDPWD/dotfiles/zshrc .zshrc || echo ".zshrc already exists"
+[ ! -d ".zsh" ] && ln -s $OLDPWD/dotfiles/zsh .zsh  || echo ".zsh  already exists"
+[ ! -d ".gitconfig" ] && ln -s $OLDPWD/dotfiles/gitconfig .gitconfig  || echo ".gitconfig  already exists"
+[ ! -d ".bashrc" ] && ln -s $OLDPWD/dotfiles/bashrc .bashrc  || echo ".bashrc  already exists"
+[ ! -d ".config" ] && ln -s $OLDPWD/dotfiles/xdg_config .config || echo ".config already exists"
