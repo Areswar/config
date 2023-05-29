d06355de33f101a8c63150d36c7ccc90ad0728bc
Pierre-Alexandre Veyry <paveyry@arista.com>
2023-05-29 17:05:26 +0100

    dotfiles: xdg: nvim: make paq autoclone

--- dotfiles/xdg_config/nvim/init.lua
+++ dotfiles/xdg_config/nvim/init.lua
@@ -91,8 +91,31 @@ vim.api.nvim_set_keymap('n', '<down>', '<nop>', {silent = true})
 vim.api.nvim_set_keymap('n', '<left>', '<nop>', {silent = true})
 vim.api.nvim_set_keymap('n', '<right>', '<nop>', {silent = true})
 
-require 'paq' {
-    'savq/paq-nvim',
+local function clone_paq()
+    local path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+    local is_installed = vim.fn.empty(vim.fn.glob(path)) == 0
+    if not is_installed then
+        vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
+        return true
+    end
+end
+
+local function bootstrap_paq(packages)
+    local first_install = clone_paq()
+    vim.cmd.packadd("paq-nvim")
+    local paq = require("paq")
+    if first_install then
+        vim.notify("Installing plugins... If prompted, hit Enter to continue.")
+    end
+
+    -- Read and install packages
+    paq(packages)
+    paq.install()
+end
+
+  -- Call helper function
+  bootstrap_paq {
+    "savq/paq-nvim",
 
     -- Language server for neovim
     'neovim/nvim-lspconfig',
